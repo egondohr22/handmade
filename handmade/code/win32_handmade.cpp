fec28dc3d1b2c2f64d513985b6edde824cf58653
egondohr <egondohr03@gmail.com>
2023-11-05 23:19:14 +0200

    Partial window setup

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,11 +1,50 @@
 #include <Windows.h>
 
+LRESULT CALLBACK MainWindowCallback(
+HWND hwnd, 
+UINT uMsg, 
+WPARAM wParam, 
+LPARAM lParam ) {
+    switch (uMsg)
+    {
+        case WM_SIZE: {
+
+        }
+        break;
+
+        case WM_DESTROY: {
+
+        }
+        break;
+
+        case WM_CLOSE: {
+
+        }
+        break;
+
+        case WM_ACTIVATEAPP: {
+
+        }
+        break;
+        
+        default:
+            break;
+    }
+}
+
 int CALLBACK WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  LPSTR lpCmdLine,
-  int nCmdShow
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow
 ) {
-  MessageBoxA(0, "Test Text", "Test Title", MB_OK|MB_ICONINFORMATION);
-  return 0;
+    WNDCLASS WindowClass = {};
+
+    WindowClass.style = CS_OWNDC; 
+    WindowClass.lpfnWndProc = 0; 
+    WindowClass.hInstance = hInstance; 
+    // WindowClass.hIcon; 
+    WindowClass.lpszClassName = L"HandmadeHeroWindowClass";
+
+    return 0;
 }
